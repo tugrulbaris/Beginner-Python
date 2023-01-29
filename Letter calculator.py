5b4c47e1be4146e270857441153cbcc920e3c4df
Tuğrul Barış <119431665+bitrays@users.noreply.github.com>
2023-01-29 08:19:11 +0300

    Delete Letter calculator.py

--- Letter calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the special letter calculator!")
-print("I can calculate how many letters are in your story, let's start.")
-name = input("What is the name of the story?")
-text = input("Please enter your story: ")
-
-lenght = len(text)
-
-print("This is how much letters", (name),"contain:", (lenght))
-print("See you later!")
