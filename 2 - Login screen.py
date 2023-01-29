c9fa7ddba12c63fe634aae53feba09ca0c549779
Tuğrul Barış <119431665+bitrays@users.noreply.github.com>
2023-01-29 08:20:10 +0300

    I tried to make login screen.

---
+++ 2 - Login screen.py
@@ -0,0 +1,7 @@
+password = input("Please enter your password: ")
+
+while password != "yourpassword" :
+    print("Password was wrong, please try again.")
+    password = input("Please enter your password: ")
+
+print("Password is correct. Welcome again!")
